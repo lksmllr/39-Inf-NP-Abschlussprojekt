1f09e96bdc960c1d1e3105cfdb0bd1e067d3eab1
lksmllr <lmueller@techfak.uni-bielefeld.de>
2018-06-12 11:24:07 +0200

    refs #2 did django tutorial and tried to understand how it all works ...

--- src/server/server/activeMembers/models.py
+++ src/server/server/activeMembers/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class activeMembers(models.Model):
+    mac_adress = models.CharField(max_length=100)
+    ping_date = models.DateTimeField('ping date')
