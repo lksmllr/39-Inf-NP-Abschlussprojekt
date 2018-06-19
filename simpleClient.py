b640b33a6d2c6c8894bc5910bab9bb37ee55d8da
lksmllr <lmueller@techfak.uni-bielefeld.de>
2018-06-19 11:11:39 +0200

    keine ahnung wie und womit ich anfangen soll

--- simpleClient.py
+++ simpleClient.py
@@ -1,12 +1,39 @@
-import requests
+#!/usr/bin/env python3
+
 import socket
+import sys
+import argparse
 
-#r = requests.get('0.0.0.0:8000/README.me')
+# parse arguments
+parser = argparse.ArgumentParser(description="FastaGenScript")
+parser.add_argument("host", type=str, help="HostName")
+parser.add_argument("port", type=str, help="Port")
+parser.add_argument("command", type=str, help="Command")
+args = parser.parse_args()
 
-s = socket.socket(
-    socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('0.0.0.0', 8000))
-s.send('README.me'.encode('utf-8'))
-response = s.recv(1024)
+# establish connection
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+sockaddr = (args.host, int(args.port));
+s.connect(sockaddr)
 
-print(str(response))
+messageAsLines = args.command.split()
+i = 0
+closed = False
+while ( (not s.timeout) or (not closed) ):
+    # send bytes to server
+    if (i < len(messageAsLines)):
+        print("Sending now line "+str(i)+" to Server: "+messageAsLines[i])
+        sentbytes = s.send(messageAsLines[i].encode('utf-8'))
+    # read bytes from server
+    if (i < len(messageAsLines)):
+        print("Waiting for Server to receive line")
+        buffersize = len(messageAsLines[i].encode('utf-8'))
+        recbytes = s.recv(buffersize)
+        recbytes = recbytes.decode('utf-8')
+        print("Response from "+args.host+", "+args.port+": "+recbytes)
+    elif (len(messageAsLines) == i):
+        closed = True
+    i = i + 1
+
+# close connection
+s.close()
