0fe3c0282b82db2b3423a492aba9f237177f12a5
lksmllr <lmueller@techfak.uni-bielefeld.de>
2018-06-21 14:51:38 +0200

    refs #2 server error. irgendwie kann ich die daten aus dem request nicht abfragen

--- thinClient_client/thinClient_client.py
+++ thinClient_client/thinClient_client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-import requests
+import requests, json
 
-r = requests.post("http://localhost:5000/heartbeat"
-    , data={'id': 'macbook'})
+r = requests.post("http://127.0.0.1:5000/heartbeat/"
+    , json.dumps({'id': 'macbook'}))
 print(r.status_code, r.reason)
--- thinClient_server/heartbeat.py
+++ thinClient_server/heartbeat.py
@@ -1,17 +1,18 @@
 import functools
 import datetime
+from http import HTTPStatus
 
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 from werkzeug.security import check_password_hash, generate_password_hash
+from thinClient_server.db import get_db
 
-from flaskr.db import get_db
 
 bp = Blueprint('heartbeat', __name__, url_prefix='/heartbeat')
 
-@bp.route('/heartbeat', methods=('GET', 'POST'))
-def register():
+@bp.route('/', methods=('GET', 'POST'))
+def latest_heartbeat():
     if request.method == 'POST':
         thinClient_id = request.form['id']
         db = get_db()
@@ -21,13 +22,18 @@ def register():
             error = 'thinClient_id is required.'
 
         elif db.execute(
-            'SELECT id FROM thinClients WHERE id = ?', (thinClient_id,)
+            'SELECT latest_heartbeat FROM thinClients WHERE id = ?', (thinClient_id,)
         ).fetchone() is not None:
             error = 'ThinClient {} is already registered.'.format(thinClient_id)
 
         if error is None:
             db.execute(
                 'INSERT INTO thinClients (thinClient_id,latest_heartbeat) VALUES (?,?)',
-                (thinClient_id, datetime.datetime.now()).split('.')[0])
+                (thinClient_id, datetime.datetime.now()).split('.')[0]
             )
             db.commit()
+            return HTTPStatus.OK
+
+        flash(error)
+
+    return content, HTTPStatus.BAD_REQUEST
