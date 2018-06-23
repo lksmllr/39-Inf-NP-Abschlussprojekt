5cf9ca9a18de2a21dfc48876f673b53b05bcde37
lksmllr <lmueller@techfak.uni-bielefeld.de>
2018-06-23 15:46:16 +0200

    refs #4 client sendet nun parallel heartbeat zu server

--- thinClient_client/thinClient_client.py
+++ thinClient_client/thinClient_client.py
@@ -3,76 +3,13 @@
 import requests, json, uuid
 import os, sys, platform, subprocess
 import argparse, shlex
+import time
 from sh import cd
+from threading import Thread
 
 thinClient_server_heartbeat_URL = 'http://127.0.0.1:5000/heartbeat/'
 wrong_args = 'Type -h for help!'
 quit_ui = False
-
-# shell like loop for user interactions and or commands
-def user_interface():
-    global quit_ui, wrong_args
-
-    print("\033[H\033[J")
-    print('Welcome to your ThinClient- Shell!')
-    print('\nType -h or --help for usage information.\n')
-
-    while(quit_ui is not True):
-        sys_path = os.getcwd()
-        sys_path = sys_path.split('/')
-        my_path = '/'+sys_path[len(sys_path)-1]
-
-        user_input = input(my_path+" ThinClientShell>>")
-        try:
-            parser = argparse.ArgumentParser(description="ThinClientShell")
-            parser.add_argument("-q", "--quit", action="store_true"
-            , default=False, help="Quit ThinClientShell")
-            parser.add_argument("-cd", help="Change working directory")
-
-            # package functions
-            parser.add_argument("-p", "--packages", action="store_true"
-            , help="List available packages")
-            parser.add_argument("-u", "--update"
-            , help="Update Package with package_id")
-            parser.add_argument("-i", "--install"
-            , help='Install package with id package_id')
-
-            # client functions
-            parser.add_argument("-lc", "--listclients"
-            , help="List all server known clients")
-            parser.add_argument("-a", "--alive"
-            , help="Online status of client with id client_id")
-            parser.add_argument("-inf", "--info"
-            , help="Information about client with id client_id")
-
-            args, unknown = parser.parse_known_args(shlex.split(user_input))
-
-            if args.quit:
-                print('\nGood Bye &nd have a great day!\n')
-                quit_ui = True
-            elif args.packages:
-                list_packages()
-                print('Package- list')
-            elif args.update:
-                update(args.update)
-                print('updating ...')
-            elif args.install:
-                upgrade(args.install)
-                print('installing '+args.install+'...')
-            elif args.cd:
-                cd(args.cd)
-                """
-                maybe auto completion, see
-                https://edwards.sdsu.edu/research/autocompletion-in-default-python-shell/
-                """
-            else:
-                try:
-                    subprocess.run(user_input, shell=True, check=False)
-                except SystemExit as e:
-                    pass
-
-        except SystemExit as e:
-            pass
 
 # update package with id package_id
 def update(package_id):
@@ -125,13 +62,82 @@ def send_heartbeat():
     my_cpu = get_cpu()
     my_ram = get_ram()
     my_gpu = get_gpu()
-    print(get_ram())
-    r = requests.post(thinClient_server_heartbeat_URL
-        , data={'id': my_id, 'cpu': my_cpu, 'ram': my_ram, 'gpu': my_gpu})
-    if r.status_code is 200:
-        print('Client: '+get_mac_adress()
-        +' sent heartbeat to: '+thinClient_server_heartbeat_URL)
-    print(r.status_code, r.reason)
+    while(quit_ui is not True):
+        r = requests.post(thinClient_server_heartbeat_URL
+            , data={'id': my_id, 'cpu': my_cpu, 'ram': my_ram, 'gpu': my_gpu})
+        #if r.status_code is 200:
+            #print('Client: '+get_mac_adress()+' sent heartbeat to: '+thinClient_server_heartbeat_URL)
+        #print(r.status_code, r.reason)
+        time.sleep(1)
 
-#send_heartbeat()
+# main loop for user interactions and or commands (shell-like)
+# offers use of system commands like ls, cd
+def user_interface():
+    global quit_ui, wrong_args, t
+
+    t.start()
+
+    print("\033[H\033[J")
+    print('Welcome to your ThinClient- Shell!')
+    print('\nType -h or --help for usage information.\n')
+
+    while(quit_ui is not True):
+        sys_path = os.getcwd()
+        sys_path = sys_path.split('/')
+        my_path = '/'+sys_path[len(sys_path)-1]
+
+        user_input = input(my_path+" ThinClientShell>>")
+        try:
+            parser = argparse.ArgumentParser(description="ThinClientShell")
+            parser.add_argument("-q", "--quit", action="store_true"
+            , default=False, help="Quit ThinClientShell")
+            parser.add_argument("-cd", help="Change working directory")
+
+            # package functions
+            parser.add_argument("-p", "--packages", action="store_true"
+            , help="List available packages")
+            parser.add_argument("-u", "--update"
+            , help="Update Package with package_id")
+            parser.add_argument("-i", "--install"
+            , help='Install package with id package_id')
+
+            # client functions
+            parser.add_argument("-lc", "--listclients"
+            , help="List all server known clients")
+            parser.add_argument("-a", "--alive"
+            , help="Online status of client with id client_id")
+            parser.add_argument("-inf", "--info"
+            , help="Information about client with id client_id")
+
+            args, unknown = parser.parse_known_args(shlex.split(user_input))
+
+            if args.quit:
+                print('\nGood bye &nd have a great day!\n')
+                quit_ui = True
+            elif args.packages:
+                list_packages()
+                print('Package- list')
+            elif args.update:
+                update(args.update)
+                print('updating ...')
+            elif args.install:
+                upgrade(args.install)
+                print('installing '+args.install+'...')
+            elif args.cd:
+                cd(args.cd)
+                """
+                maybe auto completion, see
+                https://edwards.sdsu.edu/research/autocompletion-in-default-python-shell/
+                """
+            else:
+                try:
+                    subprocess.run(user_input, shell=True, check=False)
+                except SystemExit as e:
+                    # dont raise error
+                    pass
+        except SystemExit as e:
+            # dont raise error
+            pass
+
+t = Thread(target=send_heartbeat)
 user_interface()
