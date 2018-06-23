f6a29cc6b6edfe53d2102814d03e447862311569
lksmllr <lmueller@techfak.uni-bielefeld.de>
2018-06-23 13:49:06 +0200

    refs #10 nutzerinteraktion und hilfe seiten nun implementiert

--- thinClient_client/thinClient_client.py
+++ thinClient_client/thinClient_client.py
@@ -2,8 +2,64 @@
 
 import requests, json, uuid
 import os, sys, platform
+import argparse, shlex
 
 thinClient_server_heartbeat_URL = 'http://127.0.0.1:5000/heartbeat/'
+wrong_args = 'Type -h for help!'
+quit_ui = False
+
+# shell like loop for user interactions and or commands
+def user_interface():
+    global quit_ui, wrong_args
+
+    print("\033[H\033[J")
+    print('Welcome to your ThinClient- Shell!')
+    print('\nType -h or --help for usage information.\n')
+
+    while(quit_ui is not True):
+        user_input = input("ThinClientShell>>")
+        try:
+            parser = argparse.ArgumentParser(description="ThinClientShell")
+            parser.add_argument("-q", "--quit", action="store_true", default=False, help="Quit ThinClientShell")
+            parser.add_argument("-p", "--packages", action="store_true", help="List available packages")
+            parser.add_argument("-u", "--update", help="Update Package with package_id")
+            args = parser.parse_args(shlex.split(user_input))
+
+            if args.quit:
+                quit_ui = True
+            elif args.packages:
+                list_packages()
+                print('Package- list')
+            elif args.update:
+                update(args.update)
+                print('updating ...')
+
+        except SystemExit:
+            pass
+
+# update package with id package_id
+def update(package_id):
+    pass
+
+# install package with id package_id
+def upgrade(package_id):
+    pass
+
+# list available packages
+def list_packages():
+    pass
+
+# show information about other client
+def show(client_id):
+    pass
+
+# ask server if specific client is online now
+def alive(client_id):
+    pass
+
+# list all ThinClients
+def list_clients():
+    pass
 
 # returns mac adress of this machine
 def get_mac_adress():
@@ -40,4 +96,5 @@ def send_heartbeat():
         +' sent heartbeat to: '+thinClient_server_heartbeat_URL)
     print(r.status_code, r.reason)
 
-send_heartbeat()
+#send_heartbeat()
+user_interface()
